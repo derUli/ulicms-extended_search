814c44e490c469bcc0ad99c23257c5fc253236f8
Ulrich Schmidt <u.schmidt@velian.de>
2016-12-05 14:47:36 +0100

    klasse form-control entfernt

--- extended_search/src/content/modules/extended_search/templates/search.php
+++ extended_search/src/content/modules/extended_search/templates/search.php
@@ -12,13 +12,13 @@ if (! empty ( $_GET ["q"] )) {
 	<label for="q"><?php translate("search_subject")?></label> <input
 		type="search" required="true" name="q"
 		value="<?php  Template::escape($subject);?>" results="10"
-		autosave="<?php echo md5 ( $_SERVER ["SERVER_NAME"] );?>"
-		class="form-control"> <input type="submit"
-		value="<?php translate("submit");?>" class="form-control">
+		autosave="<?php echo md5 ( $_SERVER ["SERVER_NAME"] );?>"> <input type="submit"
+		value="<?php translate("submit");?>">
 
 </form>
 <?php if($result){?>
 <?php if(count($result) > 0){?>
+<p>
 <ol class="search-subjects">
 <?php
 		foreach ( $result as $dataset ) {
@@ -27,10 +27,11 @@ if (! empty ( $_GET ["q"] )) {
 <?php }?>
 
 	</ol>
+</p>
 <?php }?>
 <?php
 } else {
 	?>
-	translate("no_results_found");
-	<?php
+<p><?php translate("no_results_found");?></p>
+<?php
 }
