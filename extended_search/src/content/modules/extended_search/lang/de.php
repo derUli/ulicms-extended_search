532aaca42f2252aefe9346908a4af7e5be881e07
Ulrich Schmidt <admin@deruli.de>
2017-01-14 15:12:48 +0100

    title_filter eingebaut

--- extended_search/src/content/modules/extended_search/lang/de.php
+++ extended_search/src/content/modules/extended_search/lang/de.php
@@ -2,3 +2,4 @@
 define ( "TRANSLATION_SEARCH_SUBJECT", "Suchbegriff: " );
 define ( "TRANSLATION_SUBMIT", "Absenden" );
 define ( "TRANSLATION_NO_RESULTS_FOUND", "Keine Ergebnisse gefunden." );
+define ( "TRANSLATION_SEARCH_RESULTS_FOR_X", "Suchergebnisse für \"%subject%\"" );
--- extended_search/src/content/modules/extended_search/lang/en.php
+++ extended_search/src/content/modules/extended_search/lang/en.php
@@ -2,3 +2,4 @@
 define ( "TRANSLATION_SEARCH_SUBJECT", "Subject: " );
 define ( "TRANSLATION_SUBMIT", "Absenden" );
 define ( "TRANSLATION_NO_RESULTS_FOUND", "No results found." );
+define ( "TRANSLATION_SEARCH_RESULTS_FOR_X", "Search Result for \"%subject%\"" );
