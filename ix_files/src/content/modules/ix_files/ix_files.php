6878e5d79f211c2fffc2141785a47776b9f42083
Ulrich Schmidt <admin@deruli.de>
2017-08-11 17:16:49 +0200

    nun sind auch dateinamen suchbar

--- ix_files/src/content/modules/ix_files/ix_files.php
+++ ix_files/src/content/modules/ix_files/ix_files.php
@@ -1,47 +1,59 @@
 <?php
-class IXFiles extends Indexer {
-	public function doIndex() {
-		$controller = ControllerRegistry::get ( "SearchController" );
-		if (! $controller) {
-			return;
-		}
-		$contentFolder = Path::resolve ( "ULICMS_ROOT/content/files" );
-		$files = find_all_files ( $contentFolder );
-		$languages = getAllLanguages ();
-		foreach ( $files as $file ) {
-			$content = $this->getFileContent ( $file );
-			if ($content) {
-				$url = str_replace ( ULICMS_ROOT, '', $file );
-				foreach ( $languages as $language ) {
-					$identifier = "file/" . $language . "/" . md5 ( $url );
-					$title = basename ( $file );
-					$controller->saveDataset ( $identifier, $url, $title, $content, $language );
-				}
-			}
-		}
-	}
-	protected function getFileContent($file) {
-		$content = null;
-		$extension = strtolower ( pathinfo ( $file, PATHINFO_EXTENSION ) );
-		switch ($extension) {
-			case "doc" :
-				$content = $this->docToText ( $file );
-				break;
-			case "txt" :
-				$content = file_get_contents ( $file );
-				break;
-			case "pdf" :
-				// @TODO: Implented pdf indexing
-				break;
-			default :
-				// default is null
-				break;
-		}
-		return $content;
-	}
-	public function docToText($file) {
-		$pathToAntiword = apply_filter ( "/usr/bin/antiword", "path_to_antiword" );
-		$cmd = "$pathToAntiword " . escapeshellarg ( $file );
-		return shell_exec ( $cmd );
-	}
+
+class IXFiles extends Indexer
+{
+
+    public function doIndex()
+    {
+        $controller = ControllerRegistry::get("SearchController");
+        if (! $controller) {
+            return;
+        }
+        $contentFolder = Path::resolve("ULICMS_ROOT/content/files");
+        $files = find_all_files($contentFolder);
+        $languages = getAllLanguages();
+        foreach ($files as $file) {
+            $content = $this->getFileContent($file);
+            if ($content) {
+                $url = str_replace(ULICMS_ROOT, '', $file);
+                foreach ($languages as $language) {
+                    $fulltext = $file . " " . basename($file) . " " . pathinfo($file, PATHINFO_FILENAME) . pathinfo($file, PATHINFO_FILENAME) . " " . $content;
+                    $identifier = "file/" . $language . "/" . md5($url);
+                    $title = basename($file);
+                    $controller->saveDataset($identifier, $url, $title, $fulltext, $language);
+                }
+            }
+        }
+    }
+
+    protected function getFileContent($file)
+    {
+        $content = null;
+        $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        switch ($extension) {
+            case "doc":
+                $content = $this->docToText($file);
+                break;
+            case "docx":
+                // @TODO: Implented docx indexing
+                break;
+            case "txt":
+                $content = file_get_contents($file);
+                break;
+            case "pdf":
+                // @TODO: Implented pdf indexing
+                break;
+            default:
+                // default is null
+                break;
+        }
+        return $content;
+    }
+
+    public function docToText($file)
+    {
+        $pathToAntiword = apply_filter("/usr/bin/antiword", "path_to_antiword");
+        $cmd = "$pathToAntiword " . escapeshellarg($file);
+        return shell_exec($cmd);
+    }
 }
