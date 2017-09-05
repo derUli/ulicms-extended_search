ce95f4341c8560d439c9bcca04ca616bd936f36c
Ulrich Schmidt <admin@deruli.de>
2017-09-05 06:28:21 +0200

    numerische HTML-Entities werden nun ersetzt.

--- ix_files/src/content/modules/ix_files/ix_files.php
+++ ix_files/src/content/modules/ix_files/ix_files.php
@@ -73,6 +73,10 @@ class IXFiles extends Indexer
         
         if (is_string($content)) {
             $content = preg_replace('/\n(\s*\n)+/', "\n", $content); // Quotes are important here.
+            $content = preg_replace_callback('/&#([0-9a-fx]+);/mi', function ($ord) {
+                return $this->replaceNumEntity($ord);
+            }, $content);
+            
             $content = trim($content);
         }
         return $content;
@@ -129,5 +133,69 @@ class IXFiles extends Indexer
         $converter->decodePDF();
         $content = $converter->output();
         return $content;
+    }
+
+    private function replaceNumEntity($ord)
+    {
+        $ord = $ord[1];
+        if (preg_match('/^x([0-9a-f]+)$/i', $ord, $match)) {
+            $ord = hexdec($match[1]);
+        } else {
+            $ord = intval($ord);
+        }
+        
+        $no_bytes = 0;
+        $byte = array();
+        
+        if ($ord < 128) {
+            return chr($ord);
+        } elseif ($ord < 2048) {
+            $no_bytes = 2;
+        } elseif ($ord < 65536) {
+            $no_bytes = 3;
+        } elseif ($ord < 1114112) {
+            $no_bytes = 4;
+        } else {
+            return;
+        }
+        
+        switch ($no_bytes) {
+            case 2:
+                {
+                    $prefix = array(
+                        31,
+                        192
+                    );
+                    break;
+                }
+            case 3:
+                {
+                    $prefix = array(
+                        15,
+                        224
+                    );
+                    break;
+                }
+            case 4:
+                {
+                    $prefix = array(
+                        7,
+                        240
+                    );
+                }
+        }
+        
+        for ($i = 0; $i < $no_bytes; $i ++) {
+            $byte[$no_bytes - $i - 1] = (($ord & (63 * pow(2, 6 * $i))) / pow(2, 6 * $i)) & 63 | 128;
+        }
+        
+        $byte[0] = ($byte[0] & $prefix[0]) | $prefix[1];
+        
+        $ret = '';
+        for ($i = 0; $i < $no_bytes; $i ++) {
+            $ret .= chr($byte[$i]);
+        }
+        
+        return $ret;
     }
 }
