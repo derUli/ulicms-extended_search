80a497f61c914c13f3d1b2b3863a04694798fbbe
Ulrich Schmidt <u.schmidt@velian.de>
2018-01-29 13:21:32 +0100

    php eingerückt

--- ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
+++ ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
@@ -1,23 +1,25 @@
 <?php
-class IXFullCalendar extends Indexer {
-	public function doIndex() {
-		$controller = ControllerRegistry::get ( "SearchController" );
-		if (! $controller) {
-			return;
-		}
+class IXFullCalendar extends Indexer
+{
+    public function doIndex()
+    {
+        $controller = ControllerRegistry::get("SearchController");
+        if (!$controller) {
+            return;
+        }
 
-  $query = Database::query("select id, title, url from `{prefix}events` where url is not null and url <> '' and url <> 'http://' and url <> 'https://'", true);
-	while($row = Database::fetchObject($query)){
+        $query = Database::query("select id, title, url from `{prefix}events` where url is not null and url <> '' and url <> 'http://' and url <> 'https://'", true);
+        while ($row = Database::fetchObject($query)) {
 
-		$url = $row->url;
-		$title = $row->title;
-		$content = "{$row->title} {$row->url}";
-		$languages = getAllUsedLanguages();
-		foreach($languages as $language){
-			$identifier = "events/{$row->id}/{$language}";
-		   $controller->saveDataset ( $identifier, $url, $title, $content, $language );
-		}
-	}
+            $url       = $row->url;
+            $title     = $row->title;
+            $content   = "{$row->title} {$row->url}";
+            $languages = getAllUsedLanguages();
+            foreach ($languages as $language) {
+                $identifier = "events/{$row->id}/{$language}";
+                $controller->saveDataset($identifier, $url, $title, $content, $language);
+            }
+        }
 
-		}
-	}
+    }
+}
