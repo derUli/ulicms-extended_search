1cea14c490b14eb4d3d0dca56eadb36ed7387ae3
Ulrich Schmidt <u.schmidt@velian.de>
2018-01-29 12:59:47 +0100

    ix_fullcalender hinzugefügt

---
+++ ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
@@ -0,0 +1,22 @@
+<?php
+class IXFullcalendar extends Indexer {
+	public function doIndex() {
+		$controller = ControllerRegistry::get ( "SearchController" );
+		if (! $controller) {
+			return;
+		}
+
+  $query = Database::query("select id, title, url from `{prefix}events` where url is not null and url <> '' and url <> 'http://' and url <> 'https://'", true);
+	while($row = Database::fetchObject($query)){
+		$identifier = "events/{$row->id}";
+		$url = $row->url;
+		$title = $row->title;
+		$content = "{$row->title} {$row->url}";
+		$languages = getAllUsedLanguage();
+		foreach($languages as $language){
+		   $controller->saveDataset ( $identifier, $url, $title, $content, $language );
+		}
+	}
+
+		}
+	}
