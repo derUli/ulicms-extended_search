57f4f1bfbe5c9696393a283e0cfe8940d176a3eb
Ulrich Schmidt <u.schmidt@velian.de>
2018-01-29 13:08:33 +0100

    language

--- ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
+++ ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
@@ -8,12 +8,13 @@ class IXFullCalendar extends Indexer {
 
   $query = Database::query("select id, title, url from `{prefix}events` where url is not null and url <> '' and url <> 'http://' and url <> 'https://'", true);
 	while($row = Database::fetchObject($query)){
-		$identifier = "events/{$row->id}";
+
 		$url = $row->url;
 		$title = $row->title;
 		$content = "{$row->title} {$row->url}";
 		$languages = getAllUsedLanguages();
 		foreach($languages as $language){
+			$identifier = "events/{$row->id}/{$language}";
 		   $controller->saveDataset ( $identifier, $url, $title, $content, $language );
 		}
 	}
