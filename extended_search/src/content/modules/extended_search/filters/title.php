660bcc46a710cd14c3e069ebc34924133df24317
Ulrich Schmidt <support@ulicms.de>
2018-06-07 07:03:11 +0200

    Zeichen im Suchstring werden nicht mehr doppelt kodiert

--- extended_search/src/content/modules/extended_search/filters/title.php
+++ extended_search/src/content/modules/extended_search/filters/title.php
@@ -2,7 +2,7 @@
 function extended_search_title_filter($title) {
 	if (! empty ( $_GET ["q"] )) {
 		return get_translation ( "search_results_for_x", array (
-				"%subject%" => Template::getEscape ( $_GET ["q"] ) 
+				"%subject%" =>  $_GET ["q"] 
 		) );
 	} else {
 		return $title;
