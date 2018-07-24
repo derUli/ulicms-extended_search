4c38c9b1582cd157b5de6fa2eb307bf4d6cb66b0
Ulrich Schmidt <support@ulicms.de>
2018-07-24 18:07:49 +0200

    ix_fullcalendar kommentiert  und eingerückt

--- ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
+++ ix_fullcalendar/src/content/modules/ix_fullcalendar/ix_fullcalendar.php
@@ -1,25 +1,30 @@
 <?php
+
 class IXFullCalendar extends Indexer
 {
+
+    // indexes all fullcalendar events with urls
     public function doIndex()
     {
         $controller = ControllerRegistry::get("SearchController");
-        if (!$controller) {
+        if (! $controller) {
             return;
         }
-
+        
+        // select all events with link urls from fullcalendar
         $query = Database::query("select id, title, url from `{prefix}events` where url is not null and url <> '' and url <> 'http://' and url <> 'https://'", true);
         while ($row = Database::fetchObject($query)) {
-
-            $url       = $row->url;
-            $title     = $row->title;
-            $content   = "{$row->title} {$row->url}";
+            
+            $url = $row->url;
+            $title = $row->title;
+            $content = "{$row->title} {$row->url}";
             $languages = getAllUsedLanguages();
+            // since events are not language dependent
+            // add a dataset to index for every language
             foreach ($languages as $language) {
                 $identifier = "events/{$row->id}/{$language}";
                 $controller->saveDataset($identifier, $url, $title, $content, $language);
             }
         }
-
     }
 }
